b126e819566ab6c66ccb3ae8c4d107fdd83c0f61
Florian Gilcher <florian.gilcher@asquera.de>
2015-04-18 02:39:43 -0700

    Some more tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -5,3 +5,23 @@ fn simple() {
     assert_eq!(edit_distance::edit_distance("kitten", "sitting"), 3);
     assert_eq!(edit_distance::edit_distance("Tier", "Tor"), 2);
 }
+
+#[test]
+fn same() {
+    assert_eq!(edit_distance::edit_distance("kitten", "kitten"), 0);
+}
+
+#[test]
+fn empty_a() {
+    assert_eq!(edit_distance::edit_distance("", "kitten"), 6);
+}
+
+#[test]
+fn empty_b() {
+    assert_eq!(edit_distance::edit_distance("sitting", ""), 7);
+}
+
+#[test]
+fn empty_both() {
+    assert_eq!(edit_distance::edit_distance("", ""), 0);
+}
