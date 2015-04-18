e60e5c093b8c59f3eead2cca4907cb069663d6db
Florian Ebeling <florian.ebeling@gmail.com>
2015-04-18 14:48:22 +0200

    Fix unicode support

--- src/lib.rs
+++ src/lib.rs
@@ -21,23 +21,27 @@
 /// ```
 pub fn edit_distance(a: &str, b: &str) -> i32 {
 
-    let row: Vec<i32> = vec![0; b.len() + 1];
-    let mut matrix: Vec<Vec<i32>> = vec![row; a.len() + 1];
+    let len_a = a.chars().count();
+    let len_b = b.chars().count();
+
+    let row: Vec<i32> = vec![0; len_b + 1];
+    let mut matrix: Vec<Vec<i32>> = vec![row; len_a + 1];
 
     // initialize string a
-    for i in (0..a.len()) {
+    for i in (0..len_a) {
         matrix[i+1][0] = matrix[i][0] + 1;
     }
 
     // initialize string b
-    for i in (0..b.len()) {
+    for i in (0..len_b) {
         matrix[0][i+1] = matrix[0][i] + 1;
     }
 
     // calculate matrix
-    for (i, ca) in a.char_indices() {
-        for (j, cb) in b.char_indices() {
-
+    let mut i = 0;
+    let mut j = 0;
+    for ca in a.chars() {
+        for cb in b.chars() {
             let alternatives = [
                 // deletion
                 matrix[i][j+1] + 1,
@@ -46,8 +50,11 @@ pub fn edit_distance(a: &str, b: &str) -> i32 {
                 // match or substitution
                 matrix[i][j] + if ca == cb { 0 } else { 1 }];
             matrix[i+1][j+1] = *alternatives.iter().min().unwrap();
+            j = j + 1;
         }
+        j = 0;
+        i = i + 1;
     }
 
-    matrix[a.len()][b.len()]
+    matrix[len_a][len_b]
 }
