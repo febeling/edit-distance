68c83f380ee25f7bc9b2dc138bfc64323fd7ac7e
Florian Ebeling <florian.ebeling@gmail.com>
2015-04-26 20:28:09 +0200

    Add a quickcheck property test

--- tests/tests.rs
+++ tests/tests.rs
@@ -41,3 +41,18 @@ fn unicode_misc_equal() {
     assert_eq!(edit_distance::edit_distance("☀☂☃☄", "☀☂☃☄"), 0);
 }
 
+extern crate quickcheck;
+use quickcheck::quickcheck;
+
+#[test]
+fn at_least_size_difference_property() {
+    fn at_least_size_difference(a: String, b: String) -> bool {
+        let size_a = a.chars().count() as i32;
+        let size_b = b.chars().count() as i32;
+        let diff = (size_a - size_b).abs();
+
+        edit_distance::edit_distance(&a, &b) >= diff
+    }
+
+    quickcheck(at_least_size_difference as fn(a: String, b: String) -> bool);
+}
